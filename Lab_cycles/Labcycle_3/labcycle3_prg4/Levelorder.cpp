e63ae0b199f2bd5b0c2f401b217c6f06b88e878b
Anjana-sankar-k <151863605+Anjana-sankar-k@users.noreply.github.com>
2024-05-04 16:45:24 +0530

    first commit

---
+++ Lab_cycles/Labcycle_3/labcycle3_prg4/Levelorder.cpp
@@ -0,0 +1,22 @@
+#include "Binarytree.cpp"
+#include <queue>
+
+template <typename T>
+void levelOrderTraversal(TreeNode<T>* root) {
+    if (root == nullptr)
+        return;
+
+    std::queue<TreeNode<T>*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode<T>* current = q.front();
+        q.pop();
+        std::cout << current->data << " ";
+
+        if (current->left != nullptr)
+            q.push(current->left);
+        if (current->right != nullptr)
+            q.push(current->right);
+    }
+}
