c8fa02a54c1a4c925bc372f703e30b0e37e734ca
Anjana-sankar-k <151863605+Anjana-sankar-k@users.noreply.github.com>
2024-05-04 15:42:50 +0530

    Add files via upload

---
+++ Lab_cycles/Labcycle_3/labcycle3_prg1/demo1.cpp
@@ -0,0 +1,27 @@
+#include "TraverseArray.cpp"
+
+int main() {
+    BinaryTree tree(10);
+
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(8);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(7);
+    tree.insert(9);
+
+    cout << "Pre-order traversal: ";
+    tree.preOrder();
+    cout << endl;
+
+    cout << "In-order traversal: ";
+    tree.inOrder();
+    cout << endl;
+
+    cout << "Post-order traversal: ";
+    tree.postOrder();
+    cout << endl;
+
+    return 0;
+}
